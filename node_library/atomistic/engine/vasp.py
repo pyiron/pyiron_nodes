3699dabf65124d50ebafdb9c9f7ce49f104ac478
Han Lin Mai <h.mai@mpie.de>
2024-08-06 03:11:35 +0200

    tests

--- node_library/atomistic/engine/vasp.py
+++ node_library/atomistic/engine/vasp.py
@@ -15,6 +15,7 @@ from ase import Atoms
 POTCAR_library_path = "/cmmc/u/hmai/vasp_potentials_54/"
 POTCAR_specification_data = "vasp_pseudopotential_PBE_data.csv"
 
+
 @dataclass
 class VaspInput:
     structure: Structure
@@ -22,6 +23,7 @@ class VaspInput:
     pseudopot_lib_path: str = field(default=POTCAR_library_path)
     potcar_paths: Optional[List[str]] = None
     kpoints: Optional[Kpoints] = None
+
 
 def is_line_in_file(filepath: str, line: str, exact_match: bool = True) -> bool:
     try:
@@ -35,42 +37,53 @@ def is_line_in_file(filepath: str, line: str, exact_match: bool = True) -> bool:
         print(f"File '{filepath}' not found.")
     return False
 
+
 def write_POSCAR(workdir: str, structure: Structure, filename: str = "POSCAR") -> str:
     poscar_path = os.path.join(workdir, filename)
     structure.to(fmt="poscar", filename=poscar_path)
     return poscar_path
+
 
 def write_INCAR(workdir: str, incar: Incar, filename: str = "INCAR") -> str:
     incar_path = os.path.join(workdir, filename)
     incar.write_file(incar_path)
     return incar_path
 
+
 def write_POTCAR(workdir: str, vasp_input, filename: str = "POTCAR") -> str:
     class PotcarNotGeneratedError(Exception):
         pass
 
     if vasp_input.potcar_paths is None:
-        potcar_paths = get_default_POTCAR_paths(vasp_input.structure, vasp_input.pseudopot_lib_path)
+        potcar_paths = get_default_POTCAR_paths(
+            vasp_input.structure, vasp_input.pseudopot_lib_path
+        )
     else:
         potcar_paths = vasp_input.potcar_paths
 
     potcar_path = os.path.join(workdir, filename)
 
-    with open(potcar_path, 'wb') as wfd:
+    with open(potcar_path, "wb") as wfd:
         for f in potcar_paths:
-            with open(f, 'rb') as fd:
+            with open(f, "rb") as fd:
                 shutil.copyfileobj(fd, wfd)
 
     return potcar_path
 
-def write_KPOINTS(workdir: str, kpoints: Optional[Kpoints] = None, filename: str = "KPOINTS") -> str:
+
+def write_KPOINTS(
+    workdir: str, kpoints: Optional[Kpoints] = None, filename: str = "KPOINTS"
+) -> str:
     kpoint_path = os.path.join(workdir, filename)
     if kpoints is not None:
         kpoints.write_file(kpoint_path)
     else:
-        with open(kpoint_path, 'w') as f:
-            f.write("Automatic mesh\n0\nGamma\n1 1 1\n0 0 0\n")  # Example KPOINTS content
+        with open(kpoint_path, "w") as f:
+            f.write(
+                "Automatic mesh\n0\nGamma\n1 1 1\n0 0 0\n"
+            )  # Example KPOINTS content
     return kpoint_path
+
 
 @Workflow.wrap.as_function_node("workdir")
 def create_WorkingDirectory(workdir: str, quiet: bool = False) -> str:
@@ -79,8 +92,11 @@ def create_WorkingDirectory(workdir: str, quiet: bool = False) -> str:
         os.makedirs(workdir)
         print(f"made directory '{workdir}'")
     else:
-        warnings.warn(f"Directory '{workdir}' already exists. Existing files may be overwritten.")
+        warnings.warn(
+            f"Directory '{workdir}' already exists. Existing files may be overwritten."
+        )
     return workdir
+
 
 @Workflow.wrap.as_function_node("workdir")
 def write_VaspInputSet(workdir: str, vasp_input) -> str:
@@ -92,20 +108,30 @@ def write_VaspInputSet(workdir: str, vasp_input) -> str:
 
     return workdir
 
+
 class Storage:
     @staticmethod
     def _convert_to_dict(instance) -> Dict:
         attributes = vars(instance)
-        result_dict = {key: value for key, value in attributes.items() if "_" not in key[0]}
+        result_dict = {
+            key: value for key, value in attributes.items() if "_" not in key[0]
+        }
         return result_dict
+
 
 class ShellOutput(Storage):
     stdout: str
     stderr: str
     return_code: int
 
+
 @Workflow.wrap.as_function_node("output")
-def run_job(command: str, workdir: str = os.getcwd(), environment: Optional[Dict[str, str]] = None, arguments: Optional[List[str]] = None) -> ShellOutput:
+def run_job(
+    command: str,
+    workdir: str = os.getcwd(),
+    environment: Optional[Dict[str, str]] = None,
+    arguments: Optional[List[str]] = None,
+) -> ShellOutput:
     if environment is None:
         environment = {}
     if arguments is None:
@@ -119,7 +145,7 @@ def run_job(command: str, workdir: str = os.getcwd(), environment: Optional[Dict
         cwd=workdir,
         encoding="utf8",
         env=environ,
-        shell=True,  
+        shell=True,
     )
     print(f"Running job in directory: {workdir}")
     output = ShellOutput()
@@ -129,50 +155,80 @@ def run_job(command: str, workdir: str = os.getcwd(), environment: Optional[Dict
     output.return_code = proc.returncode
     return output
 
+
 @Workflow.wrap.as_function_node("output_dict")
 def parse_VaspOutput(workdir: str) -> Dict:
     print(f"workdir of parse: {workdir}")
     return pvo(workdir)
 
+
 @Workflow.wrap.as_function_node("convergence")
-def check_convergence(workdir: str, filename_vasprun: str = "vasprun.xml", filename_vasplog: str = "vasp.log", backup_vasplog: str = "error.out") -> bool:
+def check_convergence(
+    workdir: str,
+    filename_vasprun: str = "vasprun.xml",
+    filename_vasplog: str = "vasp.log",
+    backup_vasplog: str = "error.out",
+) -> bool:
     converged = False
-    line_converged = "reached required accuracy - stopping structural energy minimisation"
+    line_converged = (
+        "reached required accuracy - stopping structural energy minimisation"
+    )
 
     try:
         vr = Vasprun(filename=os.path.join(workdir, filename_vasprun))
         converged = vr.converged
     except:
         try:
-            converged = is_line_in_file(filepath=os.path.join(workdir, filename_vasplog), line=line_converged, exact_match=False)
+            converged = is_line_in_file(
+                filepath=os.path.join(workdir, filename_vasplog),
+                line=line_converged,
+                exact_match=False,
+            )
         except:
             try:
-                converged = is_line_in_file(filepath=os.path.join(workdir, backup_vasplog), line=line_converged, exact_match=False)
+                converged = is_line_in_file(
+                    filepath=os.path.join(workdir, backup_vasplog),
+                    line=line_converged,
+                    exact_match=False,
+                )
             except:
                 pass
 
     return converged
 
+
 @Workflow.wrap.as_macro_node("vasp_output", "convergence_status")
-def vasp_job(self, workdir: str, vasp_input, command: str = "module load vasp; module load intel/19.1.0 impi/2019.6; unset I_MPI_HYDRA_BOOTSTRAP; unset I_MPI_PMI_LIBRARY; mpiexec -n 1 vasp_std"):
+def vasp_job(
+    self,
+    workdir: str,
+    vasp_input,
+    command: str = "module load vasp; module load intel/19.1.0 impi/2019.6; unset I_MPI_HYDRA_BOOTSTRAP; unset I_MPI_PMI_LIBRARY; mpiexec -n 1 vasp_std",
+):
     self.working_dir = create_WorkingDirectory(workdir=workdir)
     self.vaspwriter = write_VaspInputSet(workdir=workdir, vasp_input=vasp_input)
     self.job = run_job(command=command, workdir=workdir)
     self.vasp_output = parse_VaspOutput(workdir=workdir)
     self.convergence_status = check_convergence(workdir=workdir)
 
-    self.working_dir >> self.vaspwriter >> self.job >> self.vasp_output >> self.convergence_status
+    (
+        self.working_dir
+        >> self.vaspwriter
+        >> self.job
+        >> self.vasp_output
+        >> self.convergence_status
+    )
     self.starting_nodes = [self.working_dir]
     return self.vasp_output, self.convergence_status
 
+
 def stack_element_string(structure) -> Tuple[List[str], List[int]]:
-    #site_element_list = [atom.symbol for atom in atoms]
+    # site_element_list = [atom.symbol for atom in atoms]
     site_element_list = [site.species_string for site in structure]
     past_element = site_element_list[0]
     element_list = [past_element]
     element_count = []
     count = 0
-    
+
     for element in site_element_list:
         if element == past_element:
             count += 1
@@ -181,15 +237,24 @@ def stack_element_string(structure) -> Tuple[List[str], List[int]]:
             element_list.append(element)
             count = 1
             past_element = element
-    
+
     element_count.append(count)
     return element_list, element_count
 
-def get_default_POTCAR_paths(structure: Structure, pseudopot_lib_path: str, potcar_df: pd.DataFrame = pd.read_csv(POTCAR_specification_data)) -> List[str]:
+
+def get_default_POTCAR_paths(
+    structure: Structure,
+    pseudopot_lib_path: str,
+    potcar_df: pd.DataFrame = pd.read_csv(POTCAR_specification_data),
+) -> List[str]:
     ele_list, _ = stack_element_string(structure)
     potcar_paths = []
     for element in ele_list:
-        ele_default_potcar_path = potcar_df[(potcar_df["symbol"] == element) & (potcar_df["default"] == True)].potential_name.values[0]
-        potcar_paths.append(os.path.join(pseudopot_lib_path, ele_default_potcar_path, "POTCAR"))
-    
+        ele_default_potcar_path = potcar_df[
+            (potcar_df["symbol"] == element) & (potcar_df["default"] == True)
+        ].potential_name.values[0]
+        potcar_paths.append(
+            os.path.join(pseudopot_lib_path, ele_default_potcar_path, "POTCAR")
+        )
+
     return potcar_paths
---
+++ tests/unit/test_vasp.py
@@ -0,0 +1,110 @@
+import unittest
+import os
+import shutil
+from pyiron_nodes.atomistic.engine.vasp import (vasp_job, run_job, create_WorkingDirectory, VaspInput, parse_VaspOutput, 
+                           get_default_POTCAR_paths, write_POTCAR, is_line_in_file, write_POSCAR, 
+                           write_INCAR, write_KPOINTS, write_VaspInputSet, check_convergence, stack_element_string)
+from pymatgen.io.vasp.inputs import Incar, Kpoints
+from ase.build import bulk
+from pymatgen.io.ase import AseAtomsAdaptor
+from typing import List, Tuple
+import pandas as pd
+
+class TestVaspJob(unittest.TestCase):
+    def setUp(self):
+        self.incar_dict = {
+            "ENCUT": 520,
+            "EDIFF": 1e-4,
+            "ISMEAR": 1,
+            "SIGMA": 0.1,
+        }
+        self.bulk_Fe = bulk("Fe", cubic=True) * [2, 2, 2]
+        self.structure = AseAtomsAdaptor().get_structure(self.bulk_Fe)
+        self.structure[1] = "Mn"
+        self.workdir = os.path.join(os.getcwd(), "unittest_case")
+        os.makedirs(self.workdir, exist_ok=True)
+        self.incar = Incar.from_dict(self.incar_dict)
+        self.POTCAR_library_path = os.path.join("..", "resources", "vasp", "POTCAR_lib")
+        self.vasp_input = VaspInput(self.structure, self.incar, pseudopot_lib_path=self.POTCAR_library_path)
+        self.example_converged_path = os.path.join("..", "resources", "vasp", "example1")
+        self.example_failed_path = os.path.join("..", "resources", "vasp", "example2")
+        self.POTCAR_specification_data = os.path.join("..", "resources", "vasp", "POTCAR_lib", "pseudopotential_PBE_data.csv")
+
+    def tearDown(self):
+        if os.path.exists("./POTCAR"):
+            os.remove("./POTCAR")
+        if os.path.exists(self.workdir):
+            shutil.rmtree(self.workdir)
+
+    def test_write_POTCAR(self):
+        write_POTCAR(".", vasp_input=self.vasp_input)
+        self.assertTrue(os.path.exists("./POTCAR"))
+
+    def test_stack_element_string(self):
+        element_list, element_count = stack_element_string(self.structure)
+        self.assertEqual(element_list, ["Fe", "Mn", "Fe"])
+        self.assertEqual(element_count, [1, 1, 14])
+
+    def test_is_line_in_file(self):
+        test_file = "test_file.txt"
+        with open(test_file, "w") as f:
+            f.write("This is a test line.\n")
+        self.assertTrue(is_line_in_file(test_file, "This is a test line."))
+        self.assertFalse(is_line_in_file(test_file, "This is not in the file."))
+        os.remove(test_file)
+        
+    def test_create_WorkingDirectory(self):
+        create_WorkingDirectory(self.workdir)()
+        self.assertTrue(os.path.exists(self.workdir))
+        
+    def test_write_POSCAR(self):
+        poscar_path = write_POSCAR(self.workdir, self.structure)
+        self.assertTrue(os.path.exists(poscar_path))
+
+    def test_write_INCAR(self):
+        incar_path = write_INCAR(self.workdir, self.incar)
+        self.assertTrue(os.path.exists(incar_path))
+
+    def test_write_POTCAR(self):
+        potcar_path = write_POTCAR(self.workdir, self.vasp_input)
+        self.assertTrue(os.path.exists(potcar_path))
+
+    def test_write_KPOINTS(self):
+        kpoints = Kpoints.gamma_automatic([1, 1, 1])
+        kpoints_path = write_KPOINTS(self.workdir, kpoints)
+        self.assertTrue(os.path.exists(kpoints_path))
+
+    def test_write_VaspInputSet(self):
+        write_VaspInputSet(self.workdir, self.vasp_input)()
+        self.assertTrue(os.path.exists(os.path.join(self.workdir, "POSCAR")))
+        self.assertTrue(os.path.exists(os.path.join(self.workdir, "INCAR")))
+        self.assertTrue(os.path.exists(os.path.join(self.workdir, "POTCAR")))
+
+    def test_run_job(self, mock_subprocess_run):
+        output = run_job(f"cp -r {self.example_converged_path}", self.workdir)()
+        self.assertEqual(output.stdout, "stdout")
+        self.assertEqual(output.stderr, "stderr")
+        self.assertEqual(output.return_code, 0)
+
+    def test_parse_VaspOutput(self):
+        output = parse_VaspOutput(self.example_converged_path)()
+        # Assuming parse_VaspOutput returns a dictionary or similar structure
+        self.assertIsInstance(output, dict)
+        # Further checks can be added here depending on the actual structure of the output
+
+    def test_check_convergence(self):
+        converged = check_convergence(self.example_converged_path, filename_vasprun="vasprun.xml")()
+        self.assertTrue(converged)
+
+        converged = check_convergence(self.example_failed_path, filename_vasprun="vasprun.xml")()
+        self.assertFalse(converged)
+        
+    def test_get_default_POTCAR_paths(self):
+        paths = get_default_POTCAR_paths(self.structure, pseudopot_lib_path=self.POTCAR_library_path, potcar_df=pd.read_csv(self.POTCAR_specification_data))
+        expected_paths = [os.path.join(self.POTCAR_library_path, "Fe", "POTCAR"),
+                          os.path.join(self.POTCAR_library_path, "Mn_pv", "POTCAR"),
+                          os.path.join(self.POTCAR_library_path, "Fe", "POTCAR")]
+        self.assertEqual(paths, expected_paths)
+
+if __name__ == "__main__":
+    unittest.main(argv=[''], verbosity=2, exit=False)
