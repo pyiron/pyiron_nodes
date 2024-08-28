efd88043a9d8ff27936b3445e4341313d0cdf64c
liamhuber <liamhuber@greyhavensolutions.com>
2024-08-28 10:10:42 -0700

    Update math type hints and defaults

--- pyiron_nodes/math.py
+++ pyiron_nodes/math.py
@@ -4,45 +4,44 @@ For mathematical operations.
 
 from __future__ import annotations
 
-from typing import Optional
 import numpy as np
 
 from pyiron_workflow import as_function_node
 
 
 @as_function_node("sin")
-def Sin(x: Optional[list | np.ndarray] = None):
+def Sin(x: list | np.ndarray | float | int):
     return np.sin(x)
 
 
 @as_function_node("cos")
-def Cos(x: Optional[list | np.ndarray] = None):
+def Cos(x: list | np.ndarray | float | int):
     return np.cos(x)
 
 
 @as_function_node("tan")
-def Tan(x: Optional[list | np.ndarray] = None):
+def Tan(x: list | np.ndarray | float | int):
     return np.tan(x)
 
 
 @as_function_node("arcsin")
-def Arcsin(x: Optional[list | np.ndarray] = None):
+def Arcsin(x: list | np.ndarray | float | int):
     arcsin = np.arcsin(x)
     return arcsin
 
 
 @as_function_node("arccos")
-def Arccos(x: Optional[list | np.ndarray] = None):
+def Arccos(x: list | np.ndarray | float | int):
     return np.arccos(x)
 
 
 @as_function_node("arctan")
-def Arctan(x: Optional[list | np.ndarray] = None):
+def Arctan(x: list | np.ndarray | float | int):
     return np.arctan(x)
 
 
 @as_function_node("arctan2")
 def Arctan2(
-    x: Optional[list | np.ndarray] = None, y: Optional[list | np.ndarray] = None
+    x: list | np.ndarray | float | int, y: list | np.ndarray | float | int
 ):
     return np.arctan2(y, x)
