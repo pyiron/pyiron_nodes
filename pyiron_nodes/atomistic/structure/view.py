40df35cb2d69fca798dcccabcf8f203b8bc914b9
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2024-09-17 10:19:39 +0200

    Use plot3d from structuretoolkit to support ase.Atoms

--- pyiron_nodes/atomistic/structure/view.py
+++ pyiron_nodes/atomistic/structure/view.py
@@ -1,8 +1,25 @@
 from pyiron_workflow import as_function_node
 
-from ase import Atoms
+from ase import Atoms as _Atoms
+import numpy as _np
+from typing import Optional
 
 
 @as_function_node("plot")
-def Plot3d(structure: Atoms, particle_size: int = 1):
-    return structure.plot3d(particle_size=particle_size)
+def Plot3d(structure: _Atoms,
+           camera: str = "orthographic",
+           particle_size: int | float = 1.0,
+           select_atoms: Optional[_np.ndarray] = None,
+           view_plane: _np.ndarray = _np.array([0, 0, 1]),
+           distance_from_camera: int | float = 1.0,
+           ):
+    """Display atomistic structure (ase.Atoms) using nglview"""
+    from structuretoolkit import plot3d
+    return structure.plot3d(
+        structure=structure,
+        camera=camera,
+        particle_size=particle_size,
+        select_atoms=select_atoms,
+        view_plane=view_plane,
+        distance_from_camera=distance_from_camera,
+    )
