306c4725f15b6dc1166d4b4b3ec714579264cdde
Sam Waseda <waseda@mpie.de>
2025-07-30 10:21:20 +0200

    run black

--- pyiron_nodes/electrochemistry/calculators/lammps.py
+++ pyiron_nodes/electrochemistry/calculators/lammps.py
@@ -6,13 +6,21 @@ from pyiron import Project
 def CreateProject(name: str):
     return Project(name)
 
+
 @as_function_node("createjob")
-def Lammps_md(project: Project, structure, potential: str, job_name: str = 'job',
-          delete_existing_job: bool = True, temperature: int = 300,
-             n_steps: int = 100, time_step: int = 100):    
-    job = project.create.job.Lammps(job_name, delete_existing_job = delete_existing_job)
+def Lammps_md(
+    project: Project,
+    structure,
+    potential: str,
+    job_name: str = "job",
+    delete_existing_job: bool = True,
+    temperature: int = 300,
+    n_steps: int = 100,
+    time_step: int = 100,
+):
+    job = project.create.job.Lammps(job_name, delete_existing_job=delete_existing_job)
     job.structure = structure
     job.potential = potential
-    job.calc_md(temperature= temperature, n_ionic_steps= n_steps, time_step= time_step)
-    job.run(delete_existing_job = True)
-    return(job)
+    job.calc_md(temperature=temperature, n_ionic_steps=n_steps, time_step=time_step)
+    job.run(delete_existing_job=True)
+    return job
