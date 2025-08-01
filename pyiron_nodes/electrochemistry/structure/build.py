6d90167f3c8994dd419b9451e213d900e1e5b962
ahmed abdelkawy <a.abdelkawy@mpie.de>
2025-08-01 16:40:11 +0200

    adding function to solvate orthogonal surfaces

--- pyiron_nodes/electrochemistry/structure/build.py
+++ pyiron_nodes/electrochemistry/structure/build.py
@@ -1,4 +1,4 @@
-from pyiron_workflow import Workflow
+from pyiron_workflow import Workflow, as_function_node
 import ase.units as units
 
 
@@ -28,3 +28,38 @@ def build_water(project, n_mols: int = 10):
     )
     water.set_repeat([n, n, n])
     return water
+
+
+@as_function_node
+def add_water_film(electrode, water_width: float = 10.0, hydrophobic_gap: float = 3.0):
+
+    from ase.build import molecule
+    from pyiron_atomistics import ase_to_pyiron
+    import ase.units as units
+    import numpy as np
+    
+    lx, ly = electrode.cell.diagonal()[:2]
+    zmin = np.max(electrode.positions[:,2])
+    zmax = zmin + water_width
+    
+    # Water 
+    n_mols = 1
+    density = 1.0e-24  # g/A^3
+    mol_mass_water = 18.015  # g/mol
+    
+    # Determining the supercell size size
+    mass = mol_mass_water * n_mols / units.mol  # g
+    vol_h2o = mass / density  # in A^3
+    a = vol_h2o ** (1.0 / 3.0)  # A
+    cell = np.array([lx, ly, water_width])
+    cell_repeat = (cell // a).astype(int)
+    
+    H2O = molecule('H2O',cell=cell/cell_repeat)
+    H2O.set_pbc(True)
+    H2O = ase_to_pyiron(H2O).repeat(cell_repeat)
+    H2O.positions[:,2] += zmin + hydrophobic_gap
+    H2O.set_cell(electrode.cell)
+
+    electrochemical_cell = electrode + H2O
+
+    return electrochemical_cell
