9fb06132527154c66f25880204f3f008a98d0f89
Prabhath C <prabhath.21n@gmail.com>
2026-06-08 17:09:40 +0200

    Set python version to 3.12 and update smoke test to use unittest

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,2 +1,8 @@
-def test_smoke():
-    assert True
+import unittest
+
+class TestSomething(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(1, 1)
+
+if __name__ == "__main__":
+    unittest.main()
