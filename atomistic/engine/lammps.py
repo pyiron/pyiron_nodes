a279c5bfe35028413fe5d175094d67b8126de8c8
Sriram Anand <sanand@cmti001.bc.mpcdf.mpg.de>
2026-06-17 14:16:44 +0200

    MAnually fixed some merge conflicts. Need to revisit as to why the timestep is not being recorded as the value from the dataclass, presently the CreateLammpsInput node seems to use the default timestep.

--- atomistic/engine/lammps.py
+++ atomistic/engine/lammps.py
@@ -31,6 +31,8 @@ class LammpsIOBundle:
     lammps_input_filename: str = "lmp.in"
     lammps_structure_string: str = ""
     lammps_structure_filename: str = "lammps.data"
+    lammps_potential_string: str = ""
+    lammps_potential_filename: str = "potential.inp"
     read_restart_filename: Optional[str] = None
     write_restart_filename: Optional[str] = None
     units: str = "metal"
@@ -40,21 +42,6 @@ from ase import Atoms
 from core import as_function_node
 from dataclasses import dataclass
 
-@dataclass
-class LammpsInputResources:
-    structure: Atoms
-    potential: str | pd.DataFrame
-    working_directory: str = "."
-    lammps_input_string: str = ''
-    lammps_input_filename: str = 'lmp.in'
-    lammps_structure_string: str = ''
-    lammps_structure_filename: str = 'lammps.data'
-    lammps_potential_string: str = ''
-    lammps_potential_filename: str = 'potential.inp'
-    read_restart_filename: Optional[str] = None
-    write_restart_filename: Optional[str] = None
-    units: str = 'metal'
-    resource_path: Optional[str] = None
 
 @as_function_node
 def ListPotentials(
@@ -127,14 +114,22 @@ def CreateLammpsStructure(
         units=units,
         resource_path=resource_path,
     )
-
     _, potential_replace, potential_elements = _get_potential(
         potential=potential, resource_path=resource_path
     )
-    
-    # CHECK if this makes sense
-    #if "atom_style" in potential_replace.keys():
-    #    atom_type = potential_replace["atom_style"].split()[-1]
+    if isinstance(potential, pd.DataFrame):
+        config_lines = potential.iloc[0]["Config"]
+        for line in config_lines:
+            line = line.strip()
+            if line.startswith("units"):
+                units = line.split()[1]
+            elif line.startswith("atom_style"):
+                atom_type = line.split()[1]
+
+    else: # Check if this works for the case where potential is a string name of the potential, not a dataframe. Should be the case for regular EAM, etc
+        if "atom_style" in potential_replace.keys():
+            atom_type = potential_replace["atom_style"].split()[-1]
+
 
     if atom_type == "full":
         # LammpsStructure does not support "full" atom_style, so an additional function write_lammps_data_full was added in this file and is used for the 'full' case
@@ -147,16 +142,14 @@ def CreateLammpsStructure(
             bond_dict=bond_dict,
             potential=potential
         )
+    else:
+        lammps_str = LammpsStructure(bond_dict=bond_dict, units=units, atom_type=atom_type)
+        lammps_str.el_eam_lst = potential_elements
+        lammps_str.structure = structure
+        structure_string = lammps_str._string_input 
 
-    # CHECK if this makes sense
-    if "atom_style" in potential_replace.keys():
-        atom_type = potential_replace["atom_style"].split()[-1]
-
-    lammps_str = LammpsStructure(bond_dict=bond_dict, units=units, atom_type=atom_type)
-    lammps_str.el_eam_lst = potential_elements
-    lammps_str.structure = structure
-
-    io_bundle.lammps_structure_string = lammps_str._string_input
+    
+    io_bundle.lammps_structure_string = structure_string
 
     return io_bundle
 
@@ -193,11 +186,160 @@ def CreateLammpsMDInput(
     potential_lst, potential_replace, _ = _get_potential(
         potential=io_bundle.potential, resource_path=io_bundle.resource_path
     )
+    # FIXME - temporary fix, should ideally use `read_restart_filename is not None`
+    # Problem gets worse when the check box is ticked and the filename is empty
+    read_restart_file = bool(read_restart_filename)
+    write_restart_file = bool(write_restart_filename)
+
+    lmp_str_lst = []
+    for l in lammps_file_initialization(
+        structure=io_bundle.structure,
+        units=io_bundle.units,
+        read_restart_file=read_restart_file,
+        restart_file=read_restart_filename,
+    ):
+        if l.strip().startswith("units") and "units" in potential_replace:
+            lmp_str_lst.append(potential_replace["units"])
+        elif l.strip().startswith("atom_style") and "atom_style" in potential_replace:
+            lmp_str_lst.append(potential_replace["atom_style"])
+            atom_type = potential_replace["atom_style"].split()[-1]
+        elif l.strip().startswith("dimension") and "dimension" in potential_replace:
+            lmp_str_lst.append(potential_replace["dimension"])
+        else:
+            lmp_str_lst.append(l)
+
+    if isinstance(io_bundle.potential, pd.DataFrame):
+        lmp_str_lst += [f"include {io_bundle.lammps_potential_filename}\n"]
+        io_bundle.lammps_potential_string = "".join(potential_lst)
+    else:
+        lmp_str_lst += potential_lst
+
+#    lmp_str_lst += potential_lst
+    lmp_str_lst += ["variable dumptime equal {} ".format(calc_kwargs.get("n_print", 1))]
+    lmp_str_lst += [
+        "dump 1 all custom ${dumptime} dump.out id type xsu ysu zsu fx fy fz vx vy vz",
+        'dump_modify 1 sort id format line "%d %d %20.15g %20.15g %20.15g %20.15g %20.15g %20.15g %20.15g %20.15g %20.15g"',
+    ]
+
+    if "n_ionic_steps" in calc_kwargs.keys():
+        n_ionic_steps = int(calc_kwargs.pop("n_ionic_steps"))
+    else:
+        n_ionic_steps = 1
+    if read_restart_file:
+        calc_kwargs["initial_temperature"] = 0.0
+    
+    
+
+    lmp_str_lst += [
+        k + " " + v
+        for k, v in set_selective_dynamics(
+            structure=io_bundle.structure, calc_md=True
+        ).items()
+    ]
+
+    calc_kwargs["units"] = io_bundle.units
+    print("calc_kwargs:", calc_kwargs)
+    lmp_str_lst += calc_md(**calc_kwargs)
+
+    if read_restart_file:
+        lmp_str_lst += ["reset_timestep 0"]
+
+    lmp_str_lst += ["run {} ".format(n_ionic_steps)]
+
+    if read_restart_file:
+        shutil.copyfile(
+            os.path.abspath(read_restart_filename),
+            os.path.join(
+                io_bundle.working_directory, os.path.basename(read_restart_filename)
+            ),
+        )
+
+    if write_restart_file:
+        lmp_str_lst.append(f"write_restart {os.path.basename(write_restart_filename)}")
+
+    io_bundle.lammps_input_string = "\n".join(lmp_str_lst)
+
+    return io_bundle
+
+    
+@as_function_node
+def RunLammpsCalculation(
+    io_bundle: LammpsIOBundle,
+    lmp_command: Optional[str] = None,
+    cores: int = 1,
+    debug: bool = False,
+):
+    # Writing
+    os.makedirs(io_bundle.working_directory, exist_ok=True)
+    with open(
+        os.path.join(io_bundle.working_directory, io_bundle.lammps_input_filename), "w"
+    ) as f:
+        f.write(io_bundle.lammps_input_string)
+
+    with open(
+        os.path.join(io_bundle.working_directory, io_bundle.lammps_structure_filename),
+        "w",
+    ) as f:
+        f.write(io_bundle.lammps_structure_string)
+
+    if isinstance(io_bundle.potential, pd.DataFrame): 
+        with open(
+            os.path.join(io_bundle.working_directory, io_bundle.lammps_potential_filename),
+            "w",
+        ) as f:
+            f.write(io_bundle.lammps_potential_string)
+    # Running
+    if not debug:
+        if lmp_command is None:
+            lmp_command = (
+                os.getenv(
+                    "ASE_LAMMPSRUN_COMMAND",
+                    f"mpiexec -n {cores} --oversubscribe lmp_mpi",
+                )
+                + f" -in {io_bundle.lammps_input_filename}"
+            )
+        result = subprocess.run(
+            lmp_command,
+            cwd=io_bundle.working_directory,
+            shell=True,
+            universal_newlines=True,
+            env=os.environ.copy(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if result.returncode != 0:
+            error_path = os.path.join(io_bundle.working_directory, "error.msg")
+            with open(error_path, "w") as f:
+                f.write(result.stdout)
+                if result.stderr:
+                    f.write(result.stderr)
+            raise RuntimeError(
+                f"LAMMPS exited with code {result.returncode}. " f"{result.stdout}"
+            )
+        output = result.stdout
+    else:
+        output = io_bundle.working_directory
+
+    return io_bundle, output
+
+
+@as_function_node
+def ParseLammpsOutput(
+    io_bundle: LammpsIOBundle,
+    dump_h5_file_name: str = "dump.h5",
+    dump_out_file_name: str = "dump.out",
+    log_lammps_file_name: str = "log.lammps",
+):
+    from lammpsparser.compatibility.file import _get_potential
+
+    _, _, species = _get_potential(
+        potential=io_bundle.potential, resource_path=io_bundle.resource_path
+    )
     output = parse_lammps_output(
-            working_directory=input_resources.working_directory,
-            structure=input_resources.structure,
+            working_directory=io_bundle.working_directory,
+            structure=io_bundle.structure,
             potential_elements=species,
-            units=input_resources.units,
+            units=io_bundle.units,
             prism=None,
             dump_h5_file_name=dump_h5_file_name,
             dump_out_file_name=dump_out_file_name,
@@ -228,7 +370,7 @@ def CreateLammpsMDInput(
         ase_trajectory = []
 
         # Get symbols once from initial structure
-        all_symbols = input_resources.structure.get_chemical_symbols()
+        all_symbols = io_bundle.structure.get_chemical_symbols()
 
         for frame_idx in range(len(out.positions)):
             
@@ -242,7 +384,6 @@ def CreateLammpsMDInput(
             ase_trajectory.append(frame)
 
     return out, ase_trajectory
-
 
 # temporary here, should be included in LammpsStructure?
 def write_lammps_data_full(
@@ -506,166 +647,7 @@ def extract_charges_from_lammps_potential(lines, specorder):
 
     print("Extracted charges:", result)
     return result
-    # FIXME - temporary fix, should ideally use `read_restart_filename is not None`
-    # Problem gets worse when the check box is ticked and the filename is empty
-    read_restart_file = bool(read_restart_filename)
-    write_restart_file = bool(write_restart_filename)
-
-    lmp_str_lst = []
-    for l in lammps_file_initialization(
-        structure=io_bundle.structure,
-        units=io_bundle.units,
-        read_restart_file=read_restart_file,
-        restart_file=read_restart_filename,
-    ):
-        if l.strip().startswith("units") and "units" in potential_replace:
-            lmp_str_lst.append(potential_replace["units"])
-        elif l.strip().startswith("atom_style") and "atom_style" in potential_replace:
-            lmp_str_lst.append(potential_replace["atom_style"])
-            atom_type = potential_replace["atom_style"].split()[-1]
-        elif l.strip().startswith("dimension") and "dimension" in potential_replace:
-            lmp_str_lst.append(potential_replace["dimension"])
-        else:
-            lmp_str_lst.append(l)
-
-    lmp_str_lst += potential_lst
-    lmp_str_lst += ["variable dumptime equal {} ".format(calc_kwargs.get("n_print", 1))]
-    lmp_str_lst += [
-        "dump 1 all custom ${dumptime} dump.out id type xsu ysu zsu fx fy fz vx vy vz",
-        'dump_modify 1 sort id format line "%d %d %20.15g %20.15g %20.15g %20.15g %20.15g %20.15g %20.15g %20.15g %20.15g"',
-    ]
-
-    lmp_str_lst += [
-        k + " " + v
-        for k, v in set_selective_dynamics(
-            structure=io_bundle.structure, calc_md=True
-        ).items()
-    ]
-    if "n_ionic_steps" in calc_kwargs.keys():
-        n_ionic_steps = int(calc_kwargs.pop("n_ionic_steps"))
-    else:
-        n_ionic_steps = 1
-    if read_restart_file:
-        calc_kwargs["initial_temperature"] = 0.0
-
-    calc_kwargs["units"] = io_bundle.units
-    lmp_str_lst += calc_md(**calc_kwargs)
-
-    if read_restart_file:
-        lmp_str_lst += ["reset_timestep 0"]
-
-    lmp_str_lst += ["run {} ".format(n_ionic_steps)]
-
-    if read_restart_file:
-        shutil.copyfile(
-            os.path.abspath(read_restart_filename),
-            os.path.join(
-                io_bundle.working_directory, os.path.basename(read_restart_filename)
-            ),
-        )
-
-    if write_restart_file:
-        lmp_str_lst.append(f"write_restart {os.path.basename(write_restart_filename)}")
-
-    io_bundle.lammps_input_string = "\n".join(lmp_str_lst)
-
-    return io_bundle
+    
 
 
-@as_function_node
-def RunLammpsCalculation(
-    io_bundle: LammpsIOBundle,
-    lmp_command: Optional[str] = None,
-    cores: int = 1,
-    debug: bool = False,
-):
-    # Writing
-    os.makedirs(io_bundle.working_directory, exist_ok=True)
-    with open(
-        os.path.join(io_bundle.working_directory, io_bundle.lammps_input_filename), "w"
-    ) as f:
-        f.write(io_bundle.lammps_input_string)
 
-    with open(
-        os.path.join(io_bundle.working_directory, io_bundle.lammps_structure_filename),
-        "w",
-    ) as f:
-        f.write(io_bundle.lammps_structure_string)
-
-    # Running
-    if not debug:
-        if lmp_command is None:
-            lmp_command = (
-                os.getenv(
-                    "ASE_LAMMPSRUN_COMMAND",
-                    f"mpiexec -n {cores} --oversubscribe lmp_mpi",
-                )
-                + f" -in {io_bundle.lammps_input_filename}"
-            )
-        result = subprocess.run(
-            lmp_command,
-            cwd=io_bundle.working_directory,
-            shell=True,
-            universal_newlines=True,
-            env=os.environ.copy(),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        if result.returncode != 0:
-            error_path = os.path.join(io_bundle.working_directory, "error.msg")
-            with open(error_path, "w") as f:
-                f.write(result.stdout)
-                if result.stderr:
-                    f.write(result.stderr)
-            raise RuntimeError(
-                f"LAMMPS exited with code {result.returncode}. " f"{result.stdout}"
-            )
-        output = result.stdout
-    else:
-        output = io_bundle.working_directory
-
-    return io_bundle, output
-
-
-@as_function_node
-def ParseLammpsOutput(
-    io_bundle: LammpsIOBundle,
-    dump_h5_file_name: str = "dump.h5",
-    dump_out_file_name: str = "dump.out",
-    log_lammps_file_name: str = "log.lammps",
-):
-    from lammpsparser.compatibility.file import _get_potential
-
-    _, _, species = _get_potential(
-        potential=io_bundle.potential, resource_path=io_bundle.resource_path
-    )
-    output = parse_lammps_output(
-        working_directory=io_bundle.working_directory,
-        structure=io_bundle.structure,
-        potential_elements=species,
-        units=io_bundle.units,
-        prism=None,
-        dump_h5_file_name=dump_h5_file_name,
-        dump_out_file_name=dump_out_file_name,
-        log_lammps_file_name=log_lammps_file_name,
-    )
-    from pyiron_nodes.atomistic.calculator.data import OutputCalcMD
-
-    out = OutputCalcMD.pure_dataclass()
-
-    out.cells = output["generic"].get("cells")
-    out.energies_tot = output["generic"].get("energies_tot")
-    out.energies_pot = output["generic"].get("energies_pot")
-    out.forces = output["generic"].get("forces")
-    out.indices = output["generic"].get("indices")
-    out.natoms = output["generic"].get("natoms")
-    out.positions = output["generic"].get("positions")
-    out.pressures = output["generic"].get("pressures")
-    out.steps = output["generic"].get("steps")
-    out.temperatures = output["generic"].get("temperature")
-    out.unwrapped_positions = output["generic"].get("unwrapped_positions")
-    out.velocities = output["generic"].get("velocities")
-    out.volumes = output["generic"].get("volume")
-    out.species = io_bundle.structure.get_chemical_symbols()
-
-    return out
